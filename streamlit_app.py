c0a7807a5612270d49139f84d677ee6489abb946
tiaraapril230428-rgb <tiaraapril230428@gmail.com>
2026-05-22 14:17:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+
+st.title("🎈 Alifia ror")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
 st.header("_Streamlit_ is :blue[cool] :sunglasses:")
 st.header("This is a header with a divider", divider="gray")
 st.header("These headers have rotating dividers", divider=True)
@@ -6,7 +11,3 @@ st.header("One", divider=True)
 st.header("Two", divider=True)
 st.header("Three", divider=True)
 st.header("Four", divider=True)
-st.title("🎈 Alifia ror")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
